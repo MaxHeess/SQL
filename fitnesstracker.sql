1d3e4c22a3f87b80249d5ddbaf0b3be8fefe58da
Maximilian Heeß <max-heess@gmx.de>
2025-05-16 13:45:34 +0200

    Add files via upload

---
+++ fitnesstracker.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    height_cm FLOAT,
+    gender ENUM('m', 'w', 'div'),
+    password VARCHAR(255)
+);
+
+CREATE TABLE weights (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    date DATE,
+    weight_kg FLOAT
+);
+
+CREATE TABLE activities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    date DATE,
+    type VARCHAR(50),
+    duration INT, 
+    calories INT
+);
